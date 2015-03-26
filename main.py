d40a6bf0f80c2fc763065bb8d07eb3f15c41d8d1
Emilio <emilio.assuncao@gmail.com>
2015-03-25 20:17:35 -0400

    Basic stuff for first line filtering.

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import bannedWords
+
+##adding functionality for reading the profanity from files, making them into things that can be passed to functions in this
+##or other modules
+def fileToLines(filename):
+    openedFile = open(filename, 'r')
+    lines = openedFile.read().splitlines()
+    openedFile.close()
+    return lines
+
+##making a function to turn a text based input from web into lines to pass to functions
+def stringToLines(string):
+    lines = string.splitlines()
+    return lines
+
+
---
+++ profaneWords.py
@@ -0,0 +1,5 @@
+##this file contains all the fucntions for removing/replacing profane words from a text
+##that is, all words that are unambiguously profane -- which are contained in the profaneWords plain text file
+
+def compare(str x):
+    pass
