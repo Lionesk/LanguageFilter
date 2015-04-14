f5fa3b87563d6b3b347ba9fb11d568c875b7a3ec
Emilio <emilio.assuncao@gmail.com>
2015-04-14 00:59:30 -0400

    Text error fix.

--- profaneWords.py
+++ profaneWords.py
@@ -3,11 +3,6 @@
 
 ##is passed a line from the main function and finds the words that are on the profane word list
 def compare(x):
-<<<<<<< HEAD
-    wordList = open('profaneWords.txt', 'r')
-    for w in wordList.readline():
-        if x.lower() == w:
-=======
     wordListRaw = open('profaneWords.txt', 'r')
     wordListRaw2 = wordListRaw.readlines()
     wordList = []
@@ -15,7 +10,6 @@ def compare(x):
     	wordList.append(wordListRaw2[line].split('\t')) # splits TSV
     for w in range(1,len(wordList)): # ignores headers
         if x == wordList[w][0]: # looks only at first 'column'
->>>>>>> d3b035fb228291c607af700ca92b19a4178498fc
             return True
     return False
 # print compare('fuck') # for debugging
