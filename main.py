dac2c0c29e833809678d93a253dbefa5e5bbea7c
Emilio <emilio.assuncao@gmail.com>
2015-04-19 22:29:05 -0400

    Updated main.py and added languagefilter.py from the CLI branch.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, os, os.path
+import sys, os
 import profaneWords, variations
 
 ##adding functionality for reading the profanity from files, making them into things that can be passed to functions in this
@@ -44,27 +44,19 @@ def censor(words):
     	censored = censored + words[w][0] + ' '
     return censored
 
-def main():
-    inputList = []
-    print "LanguageFilter Beta -- By Emilio Assuncao and Liam Bassford"
-    goodInput = False
-    while(goodInput != True):
-        # CHANGE BACK!!!
-        choice = raw_input("Type f to input a filename and s to type a string.\n").lower()
-        if(choice == 'f'):
-            fileName = 'carlin.txt'# raw_input("Enter the name of your file (must me in same directory).\n").lower()
-            if(os.path.isfile(fileName)):
-                inputList = linesToList(fileToLines(fileName))
-                goodInput = True
-            else:
-                print "File does not exist in directory."
-        elif(choice == 's'):
-            inputString = raw_input("Type the string you would like to filter.\n")
-            inputList = linesToList(stringToLines(inputString))
-            goodInput = True
-        else:
-            print "Input not recognized, please try again."
-    ##print inputList #- for debugging
+def outputToFile(filename, content):
+	# linesToList(stringToLines("Fuck your bitch ass, motherfucker")) 		# to test the list of lists
+    with open(filename, 'w') as f:
+            f.write(content)
+    print "Filtered text has been written to '%s'." % filename 
+
+def fileMain(filename, outputname):
+    inlist = linesToList(fileToLines(filename))
+    output = main(inlist)
+    print "Filtering the contents of %s, and writing them to %s." % (filename, outputname)
+    outputToFile(outputname, output)
+
+def main(inputList):
     for w in range(len(inputList)):
         if profaneWords.compare(inputList[w][0]) == True:
             inputList[w].append(True)
@@ -73,16 +65,8 @@ def main():
         else:
             inputList[w].append(False) 
     output = censor(inputList)
-#    print inputList
-   # print "Here is the filtered text."
-  #  print output
-
-# linesToList(stringToLines("Fuck your bitch ass, motherfucker")) # to test the list of lists
-    with open('results.txt', 'w') as f:
-            f.write(output)
-    print "Filtered text has been written to 'results.txt'." 
+    return output
 
 ##print linesToList(stringToLines("Fuck your bitch ass, motherfucker")) # to test the list of lists
 ##print profaneWords.compare('fuck')
-main()
 ##print censor([['Fuck',True],['your',False],['bitch',True],['ass',True]])
