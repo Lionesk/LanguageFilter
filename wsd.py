5079498a2143a9478f2af5eb202bb7614ab94455
liamrogersbassford <liam.bassford@gmail.com>
2015-04-20 17:36:22 -0400

    improvement to NB classifier
    Signed-off-by: liamrogersbassford <liam.bassford@gmail.com>

--- wsd.py
+++ wsd.py
@@ -5,6 +5,7 @@ import re
 # input: a filename and the string representing the word being searched for
 # output: a dictionary containing frequencies for each word in the token's context (max. 14 words on either side, but varies by line)
 def wordbag(f,s):
+	size = 0
 	n = 3
 	raw = open(f, 'r')
 	raw2 = raw.readlines()
@@ -15,27 +16,19 @@ def wordbag(f,s):
 	#print raw2
 	for ln in raw2:
 		for w in ln:
-			freqs[w] = 0.1
+			freqs[w] = 0.0 # initializes frequency
 	for ln in range(len(raw2)):
 		for w in range(len(raw2[ln])):
-
 			if raw2[ln][w] == s:
 				start = max(w-n,0)
 				stop = min(w+n+1,len(raw2[ln]))
 				immcont = []
 				for i in range(start,stop):
-					immcont.append(raw2[ln][i])
+					immcont.append(raw2[ln][i])	
 				# print immcont
 				for t in immcont:
 					freqs[t] = freqs[t] + 1
-#	for l in raw2:
-#		for w in l:
-#			freqs[w] = 0
-#	for l in raw2:
-#		for w in l:
-#			freqs[w] = freqs[w] + 1
-#	for f in freqs:
-#		freqs[f] = freqs[f] + 0.1
+	
 	# print freqs
 	return freqs
 
@@ -46,17 +39,35 @@ def getRatios(s):
 	f1 = 'wsd/' + s + '-1.txt'
 	c0 =  wordbag(f0,s)
 	c1 =  wordbag(f1,s)
-#	print c0
-#	print c1
+
 	for k in c0:
 		if k not in c1:
-			c1[k] = 0.1
+			c1[k] = 0.0
 	for k in c1:
-		if c1[k] != 0.1 and k not in c0:
-			c0[k] = 0.1
+		if k not in c0:
+			c0[k] = 0.0
+	for k in c0:
+		c0[k] = c0[k] + 0.1
+	for k in c1:
+		c1[k] = c1[k] + 0.1
+#	print c0
+	probs0 = {}
+	probs1 = {}
+	size0 = 0.0
+	size1 = 0.0
+	for k in c0:
+		size0 = size0 + c0[k]
+	for k in c1:
+		size1 = size1 + c1[k]
+	for k in c0:
+		probs0[k] = float(c0[k])/float(size0)
+	for k in c1:
+		probs1[k] = float(c1[k])/float(size1)
+	# print probs0
+	# print probs1
 	ratios = {}
 	for k in c0:
-		ratios[k] = float(c0[k])/float(c1[k])
+		ratios[k] = probs0[k]/probs1[k]
 	# printTop10(ratios) # if you want
 	# print ratios
 	return ratios
@@ -93,6 +104,6 @@ def assignWeights(cont, r):
 		return False
 	return True
 
-carlin = open('carlin.txt','r')
-carlin2 = carlin.read().replace('\n',' ')
-print assignWeights(carlin2.split(),getRatios('cock'))
+# carlin = open('carlin.txt','r')
+# carlin2 = carlin.read().replace('\n',' ')
+# print assignWeights(carlin2.split(),getRatios('chink'))
